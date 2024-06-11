051cea14c80d311d2475dd1bc7f03ff73e065d63
Marcos Alexandre <dev.marcosanjos@gmail.com>
2024-06-11 13:15:11 -0300

    definindo fontSize e interface

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
-import {Text as RNText, TextProps} from 'react-native';
+import {Text as RNText, TextProps, TextStyle} from 'react-native';
 
-export function Text({children}: TextProps) {
-  return <RNText>{children}</RNText>;
+export function Text({children, ...rest}: TextProps) {
+  return (
+    <RNText style={fontSize.HeadingLarge} {...rest}>
+      {children}
+    </RNText>
+  );
 }
+
+type TextVariants =
+  | 'HeadingLarge'
+  | 'HeadingMedium'
+  | 'HeadingSmall'
+  | 'ParagraphLarge'
+  | 'ParagraphMedium'
+  | 'ParagraphSmall'
+  | 'Caption'
+  | 'CaptionSmall';
+
+const fontSize: Record<TextVariants, TextStyle> = {
+  HeadingLarge: {fontSize: 32, lineHeight: 38.4},
+  HeadingMedium: {fontSize: 22, lineHeight: 26.4},
+  HeadingSmall: {fontSize: 18, lineHeight: 23.4},
+
+  ParagraphLarge: {fontSize: 18, lineHeight: 25.2},
+  ParagraphMedium: {fontSize: 16, lineHeight: 22},
+  ParagraphSmall: {fontSize: 14, lineHeight: 19.6},
+
+  Caption: {fontSize: 12, lineHeight: 16.8},
+  CaptionSmall: {fontSize: 14, lineHeight: 10},
+};
